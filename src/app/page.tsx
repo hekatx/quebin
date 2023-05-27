a742598913550e753bb123737abdf9748c41825e
eka <phyzheye01@gmail.com>
2023-05-27 09:14:19 -0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,43 @@
-import Image from 'next/image'
+import Link from "next/link";
+
+const blurredImage = {
+  width: 300,
+  height: 600,
+  filter: "blur(1.5rem)",
+  transform: "scale(1.2)",
+  backgroundImage:
+    "linear-gradient(90deg, rgb(206,204,193) 33.33333333333333%,rgb(185,182,177) 33.33333333333333% 66.66666666666666%,rgb(208,206,201) 66.66666666666666% 100%),linear-gradient(90deg, rgb(139,133,125) 33.33333333333333%,rgb(84,56,40) 33.33333333333333% 66.66666666666666%,rgb(206,196,185) 66.66666666666666% 100%),linear-gradient(90deg, rgb(143,129,117) 33.33333333333333%,rgb(138,86,51) 33.33333333333333% 66.66666666666666%,rgb(186,172,156) 66.66666666666666% 100%),linear-gradient(90deg, rgb(82,63,52) 33.33333333333333%,rgb(153,103,61) 33.33333333333333% 66.66666666666666%,rgb(105,94,77) 66.66666666666666% 100%)",
+  backgroundPosition: "0 0 ,0 33.33333333333333%,0 66.66666666666666%,0 100%",
+  backgroundSize: "100% 25%",
+  backgroundRepeat: "no-repeat",
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <section className="flex justify-between flex-col md:flex-row gap-20">
+      <div className="order-2 md:order-1 w-full flex flex-col justify-center">
+        <h1 className="font-semibold text-3xl mb-10">Hey!</h1>
+        <div className="grid gap-5">
+          <p>
+            I’m Kevin. Interested in designing interfaces that resonate with
+            users. Dedicated to precision, polish, and diverse creative
+            pursuits.
+          </p>
+          <p>
+            As well as <Link href="/craft">coding</Link>, you can find me{" "}
+            <Link href="/art">painting</Link> and{" "}
+            <Link href="/blog">writing</Link> my ideas about design, code and
+            art.
+          </p>
+          <p>
+            Currently <strong>crafting magical interfaces</strong> at{" "}
+            <strong>Capgemini</strong>
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="order-1 md:order-2 overflow-hidden w-full">
+        <div style={blurredImage} />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </section>
+  );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+function Header() {
+
+  return (
+    <header>
+      <nav>
+        <Image src="" />
+      </nav>
+    </Img>
+  )
+}
